073036cc578bc18d5f3da3512942c111007cc925
Arjun Maurya <arjun9654abc@gmail.com>
2025-08-22 15:34:12 +0530

    Update axios.js
    fix axios file

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: import.meta.env.MODE === "development" ? "https://fullstack-chat-app-ckd5.vercel.app/api" : "/api",
+  baseURL: import.meta.env.MODE === "development" ? "https://fullstack-chat-app-ckd5.vercel.app" : "/api",
   withCredentials: true,
 });
